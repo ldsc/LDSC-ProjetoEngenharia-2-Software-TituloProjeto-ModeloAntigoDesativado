1a2b4daeb17d87a9da174d9398e5e77140030cc3
andreduartebueno <andreduartebueno@gmail.com>
2015-03-27 11:25:57 -0300

    Adicionadas informações mais detalhadas sobre: i) uso do formato JAVADOC, com links externos. ii) uso do pacote listings do latex.

--- listagens/CAplicacao.cpp
+++ listagens/CAplicacao.cpp
@@ -1,10 +1,16 @@
-/**  Este programa exemplifica a estrutura/layout de um programa típico em C++  */
+//  Este programa exemplifica a estrutura de um programa típico em C++
 #include <iostream>
 
 // Inclui a declaração da classe
 #include "CAplicacao.h" 
 
-// Implementa os métodos da classe
+/** Note que no arquivo .cpp não é necessário colocar novamente a documentação
+ * que foi colocada no arquivo .h. 
+ * A documentação no arquivo .cpp costuma usar o padrão básico de C++ que é //
+ * e costuma estar mais diretamente relacionada a implementação em sí,
+ * ou seja, aos detalhes numéricos e computacionais; 
+ * detalhes e explicação das contas e da lógica computacional.
+ * */
 void CAplicacao::Run()
 {
   // std::cout escreve na tela  o texto "Bem-vindo ao C++!"
